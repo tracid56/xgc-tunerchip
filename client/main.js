136bb3926d6b99338ac50b113c474090506e068c
VoXzE <sinncere520@gmail.com>
2019-09-03 15:48:05 -0400

    Major

---
+++ client/main.js
@@ -0,0 +1,174 @@
+// Variables
+let isGuiOpen = false;
+let defaultVehicleValues = [];
+let currentVehicleTable = [];
+
+RegisterCommand("tuner", () => {
+  emit("xgc-tuner:openTuner")
+});
+
+RegisterNetEvent("xgc-tuner:openTuner")
+AddEventHandler("xgc-tuner:openTuner", () => {
+  let ped = GetPlayerPed(-1);
+  let vehicle = GetVehiclePedIsUsing(ped);
+
+  // Lets check if they're the driver
+  if (GetPedInVehicleSeat(vehicle, -1) === ped) {
+    let vehiclePlate = GetVehicleNumberPlateText(vehicle);
+    let alreadyExist = defaultVehicleValues.findIndex(e => e.plate === vehiclePlate);
+
+    if (alreadyExist < 0) {
+
+      let defaultTuneSettings = {
+        plate: vehiclePlate,
+        boost: 0,
+        acceleration: 0,
+        gearchange: 0,
+        breaking: 5,
+        drivetrain: 5,
+      }
+
+      currentVehicleTable.push(defaultTuneSettings);
+
+      defaultVehicleValues.push({
+        plate: vehiclePlate,
+        fInitialDriveForce: GetVehicleHandlingFloat(vehicle, "CHandlingData", "fInitialDriveForce"),
+        fClutchChangeRateScaleUpShift: GetVehicleHandlingFloat(vehicle, "CHandlingData", "fClutchChangeRateScaleUpShift"),
+        fClutchChangeRateScaleDownShift: GetVehicleHandlingFloat(vehicle, "CHandlingData", "fClutchChangeRateScaleDownShift"),
+        fBrakeBiasFront: GetVehicleHandlingFloat(vehicle, "CHandlingData", "fBrakeBiasFront"),
+        fDriveBiasFront: GetVehicleHandlingFloat(vehicle, "CHandlingData", "fDriveBiasFront"),
+        fInitialDragCoeff: GetVehicleHandlingFloat(vehicle, "CHandlingData", "fInitialDragCoeff"),
+        fLowSpeedTractionLossMult: GetVehicleHandlingFloat(vehicle, "CHandlingData", "fLowSpeedTractionLossMult"),
+        fDriveInertia: GetVehicleHandlingFloat(vehicle, "CHandlingData", "fDriveInertia"),
+      });
+
+      console.log(defaultVehicleValues)
+    }
+    
+    let tuneSettings = currentVehicleTable.find(e => e.plate === vehiclePlate);
+    openTunerHud(tuneSettings)
+
+  }
+
+});
+
+
+
+function openTunerHud(data) {
+  isGuiOpen = true;
+  SetNuiFocus(true, true);
+  SendNuiMessage(JSON.stringify({ type: "tunerchip-ui", display: true, tune: JSON.stringify(data) }));
+}
+
+function applyTune(data) {
+  let { boost, acceleration, gearchange, breaking, drivetrain, plate } = data; 
+  let index = currentVehicleTable.findIndex(e => e.plate === plate);
+  currentVehicleTable[index].boost = boost;
+  currentVehicleTable[index].acceleration = acceleration;
+  currentVehicleTable[index].gearchange = gearchange;
+  currentVehicleTable[index].breaking = breaking;
+  currentVehicleTable[index].drivetrain = drivetrain;
+
+  applyBoost(boost, acceleration, plate);
+  applyGearChange(gearchange, plate);
+  applyBreaks(breaking, plate);
+  applyDriveTrain(drivetrain, plate);
+}
+
+function applyGearChange(gearchange, plate) {
+  let index = currentVehicleTable.findIndex(e => e.plate === plate);  
+  let vehicle = GetVehiclePedIsUsing(GetPlayerPed(-1));
+  if (gearchange !== 0) {
+    let newScaleUp = defaultVehicleValues[index].fClutchChangeRateScaleUpShift + gearchange;
+    let newScaleDown = defaultVehicleValues[index].fClutchChangeRateScaleUpShift + gearchange;
+    let newDrag = defaultVehicleValues[index].fInitialDragCoeff + (gearchange / 50);
+    SetVehicleHandlingFloat(vehicle, "CHandlingData", "fClutchChangeRateScaleUpShift", newScaleUp)
+    SetVehicleHandlingFloat(vehicle, "CHandlingData", "fClutchChangeRateScaleDownShift", newScaleDown)
+    SetVehicleHandlingFloat(vehicle, "CHandlingData", "fInitialDragCoeff", newDrag)
+  } else {
+    SetVehicleHandlingFloat(vehicle, "CHandlingData", "fClutchChangeRateScaleUpShift", defaultVehicleValues[index].fClutchChangeRateScaleUpShift)
+    SetVehicleHandlingFloat(vehicle, "CHandlingData", "fClutchChangeRateScaleDownShift", defaultVehicleValues[index].fClutchChangeRateScaleDownShift)
+    SetVehicleHandlingFloat(vehicle, "CHandlingData", "fInitialDragCoeff", defaultVehicleValues[index].fInitialDragCoeff)
+  }
+}
+
+function applyDriveTrain(drivetrain, plate) {
+  let index = currentVehicleTable.findIndex(e => e.plate === plate);
+  let vehicle = GetVehiclePedIsUsing(GetPlayerPed(-1));
+  if (drivetrain !== 5) {
+    let newDriveTrain = (drivetrain / 10)
+    SetVehicleHandlingFloat(vehicle, "CHandlingData", "fDriveBiasFront", newDriveTrain)
+    console.log("Drive Train Set: " + newDriveTrain)
+  } else {
+    SetVehicleHandlingFloat(vehicle, "CHandlingData", "fDriveBiasFront", defaultVehicleValues[index].fDriveBiasFront)
+    console.log("Drive set to default.")
+  }
+}
+
+function applyBreaks(breaking, plate) {
+  let vehicle = GetVehiclePedIsUsing(GetPlayerPed(-1));
+  let index = defaultVehicleValues.findIndex(e => e.plate === plate)
+  if (breaking !== 5) {
+    let newBreaking = (breaking /10);
+    SetVehicleHandlingFloat(vehicle, "CHandlingData", "fBrakeBiasFront", newBreaking)
+  } else {
+    SetVehicleHandlingFloat(vehicle, "CHandlingData", "fBrakeBiasFront", defaultVehicleValues[index].fBrakeBiasFront)
+  }
+}
+
+function applyBoost(boost, acceleration, plate) {
+  let vehicle = GetVehiclePedIsUsing(GetPlayerPed(-1));
+  let index = defaultVehicleValues.findIndex(e => e.plate === plate)
+
+  if (boost === 0) {
+    SetVehicleHandlingFloat(vehicle, "CHandlingData", "fInitialDriveForce", defaultVehicleValues[index].fInitialDriveForce)
+    SetVehicleHandlingFloat(vehicle, "CHandlingData", "fLowSpeedTractionLossMult", defaultVehicleValues[index].fLowSpeedTractionLossMult)
+    console.log("Set boost and fuel to default")
+  }
+
+  let defBoost = defaultVehicleValues[index].fInitialDriveForce;
+  let defTrac = defaultVehicleValues[index].fLowSpeedTractionLossMult;
+  let newBoost = defBoost + defBoost * (boost / 200)
+  let newTraction = defTrac - defTrac * (boost / 20);
+
+  SetVehicleHandlingFloat(vehicle, "CHandlingData", "fInitialDriveForce", newBoost);
+  SetVehicleHandlingFloat(vehicle, "CHandlingData", "fLowSpeedTractionLossMult", newTraction);
+
+
+  console.log("old trac: " + defTrac)
+  console.log("New trac: " + newTraction)
+
+  if (acceleration === 0 && boost === 0) {
+    SetVehicleHandlingFloat(vehicle, "CHandlingData", "fDriveInertia", defaultVehicleValues[index].fDriveInertia)
+  } else {
+    let defaultBoost = GetVehicleHandlingFloat(vehicle, "CHandlingData", "fInitialDriveForce");
+    let newForce = (defaultBoost + defaultBoost) * (acceleration / 200);
+
+    let newInteria = defaultVehicleValues[index].fDriveInertia - defaultVehicleValues[index].fDriveInertia * (acceleration / 30);
+
+    if (newInteria < 0.5) newInteria = 0.5;
+    SetVehicleHandlingFloat(vehicle, "CHandlingData", "fInitialDriveForce", newForce)
+    SetVehicleHandlingFloat(vehicle, "CHandlingData", "fDriveInertia", newInteria)
+
+  }
+}
+
+function closeTuneGui() {
+  isGuiOpen = false;
+  SetNuiFocus(false, false);
+  SendNuiMessage(JSON.stringify({ type: "tunerchip-ui", display: false }));
+}
+
+
+RegisterNuiCallbackType("closeTuner");
+on("__cfx_nui:closeTuner", (data, cb) => {
+  closeTuneGui()
+  cb("ok")
+});
+
+RegisterNuiCallbackType("saveTune");
+on("__cfx_nui:saveTune", (data, cb) => {
+  applyTune(data);
+  closeTuneGui()
+  cb("ok")
+});
--- ui/app.js
+++ ui/app.js
@@ -1,109 +1,98 @@
 $(function() {
-  // window.onload = (e) => {
-  //   $("#container").hide();
-  //   window.addEventListener("message", (event) => {
-  //     let data = event.data;
-  //     if (data !== undefined && data.type === "Inventory-UI") {
-  //       if (data.display === true) {
-  //         $("#container").show();
-  //         console.log("heya")
-  //       } else {
-  //         $("#container").hide();
-  //       }
-  //     }
-  //   });
-  // }
 
-  // document.onkeyup = function (data) {
-  //   if (data.which == 27) {
-  //     $.post("http://xgc-inventory/closeInventory", JSON.stringify({}));
-  //     console.log("hehe")
-  //   }
-  // };
-
-  var dragDestIndex;
-  var draggedEl;
-  var draggedElIndex;
-  var rearranging = false;
-  var rearrangingDuration = 350;
-  var draggableElArr = $('.draggable');
-
-  // Update the CSS for each item to be in its correct location based on the order in the array passed
-  // @param itemEls: the DOM elements, in order, from top left, row by row
-  // @param columnsCount: the number of columns
-  // @containerEl: the DOM element of the container for the arranged elements.
-  function arrangeItems(itemEls, columnsCount, containerEl, elHeight) {
-    var $containerEl = $(containerEl);
-
-    // Set height of elements
-    var elWidth = $containerEl.width() / columnsCount;
-    elHeight = elHeight || elWidth;
-
-    for (var i = 0; i < itemEls.length; i++) {
-      var $item = $(itemEls[i]);
-      $item.data('index', i);
-      var pos = {
-        x: (i % columnsCount),
-        y: Math.floor(i / columnsCount)
-      };
-
-      $item.css({
-        top: pos.y * elHeight + 'px',
-        left: pos.x * elWidth + 'px',
-        boxSizing: 'border-box'
-      });
-    }
+  let tune = {};
+  let boostSlider = $("#boostSlider");
+  let accelerationSlider = $("#accelerationSlider");
+  let gearSlider = $("#gearSlider");
+  let breakSlider = $("#breakSlider");
+  let drivetrainSlider = $("#drivetrainSlider")
+  
+  window.onload = (e) => {
+    $("#container").hide();
+    window.addEventListener("message", (event) => {
+      let data = event.data;
+      if (data !== undefined && data.type === "tunerchip-ui") {
+        if (data.display === true) {
+          tune = JSON.parse(data.tune);
+          boostSlider.val(tune.boost);
+          accelerationSlider.val(tune.acceleration);
+          gearSlider.val(tune.gearchange);
+          breakSlider.val(tune.breaking);
+          drivetrainSlider.val(tune.drivetrain);
+          $("#container").show();
+        } else {
+          $("#container").hide();
+        }
+      }
+    });
   }
 
-  // Rearrange the items in an array, returning a the modified array.
-  // @param movedItemIndex: the index of the element being moved to a
-  //   new position in the array
-  // @param destinationIndex: the new index being given to the moved element
-  function rearrangeItems(arr, movedItemIndex, destinationIndex) {
-    var movedEl = arr.splice(movedItemIndex, 1)[0];
-    arr.splice(destinationIndex, 0, movedEl);
-    return arr;
+  // Detect when [ESC] key is clicked to close Menu
+  document.onkeyup = function (data) {
+    if (data.which == 27) {
+      $.post("http://xgc-tunerchip/closeTuner", JSON.stringify({}));
+    }
   };
 
-  arrangeItems(draggableElArr, 4, $('.content'));
-
-  $('.draggable').on('dragstart', function (e) {
-    draggedEl = $(this);
-    var sortedArr = [];
-    for (var i = 0; i < draggableElArr.length; i++) {
-      var elIndex = $(draggableElArr[i]).data('index');
-      sortedArr[elIndex] = draggableElArr[i];
-    }
-    draggableElArr = sortedArr;
-    draggedElIndex = $(this).data('index');
-    console.log('dragging element at position ', draggedElIndex);
-    $(this).css({ opacity: 0, transition: 'all 100ms ease' });
+  $("#saveButton").click(function () {
+    $.post("http://xgc-tunerchip/saveTune", JSON.stringify(tune));
   });
 
-  $('.draggable').on('dragend', function (e) {
-    e.preventDefault();
-    $(this).css({ opacity: 1, transition: 'all 700ms ease' });
+  $("#defaultTuneButton").click(function () {
+
+    // Updating on UI
+    boostSlider.val(0);
+    accelerationSlider.val(0);
+    gearSlider.val(0);
+    breakSlider.val(5);
+    drivetrainSlider.val(5);
+
+    // Updating object
+    tune.boost = JSON.parse(boostSlider.val());
+    tune.acceleration = JSON.parse(accelerationSlider.val());
+    tune.gearchange = JSON.parse(gearSlider.val());
+    tune.breaking = JSON.parse(breakSlider.val());
+    tune.drivetrain = JSON.parse(drivetrainSlider.val());
+
   });
 
-  $('.draggable').on('dragover', function (e) {
-    e.preventDefault();
-    if (rearranging) {
-      return;
-    }
-    var dragDestIndex = $(this).data('index');
-    console.log('dragging over position ', dragDestIndex);
-    draggedElIndex = draggedEl.data('index');
-    if (draggedElIndex !== dragDestIndex) {
-      rearranging = true;
-      console.log('dragging element at position ', draggedElIndex, ' to new dest at position ', dragDestIndex);
-      var rearrangedEls = rearrangeItems(draggableElArr, draggedElIndex, dragDestIndex);
-      arrangeItems(rearrangedEls, 4, $('.content'));
-      setTimeout(function () { rearranging = false }, rearrangingDuration);
-    }
+    $("#sportTuneButton").click(function () {
+
+      // Updating on UI
+      boostSlider.val(10);
+      accelerationSlider.val(10);
+      gearSlider.val(10);
+      breakSlider.val(5);
+      drivetrainSlider.val(5);
+
+      // Updating object
+      tune.boost = JSON.parse(boostSlider.val());
+      tune.acceleration = JSON.parse(accelerationSlider.val());
+      tune.gearchange = JSON.parse(gearSlider.val());
+      tune.breaking = JSON.parse(breakSlider.val());
+      tune.drivetrain = JSON.parse(drivetrainSlider.val());
+
+    });
+
+
+  boostSlider.on("input", (event) => {
+    tune.boost = JSON.parse($(event.target).val());
   });
 
-  $('.draggable').on('drop', function (e) {
-    e.preventDefault();
-    rearranging = false;
+  accelerationSlider.on("input", (event) => {
+    tune.acceleration = JSON.parse($(event.target).val());
   });
+
+  gearSlider.on("input", (event) => {
+    tune.gearchange = JSON.parse($(event.target).val());
+  });
+
+  breakSlider.on("input", (event) => {
+    tune.breaking = JSON.parse($(event.target).val());
+  });
+
+  drivetrainSlider.on("input", (event) => {
+    tune.drivetrain = JSON.parse($(event.target).val());
+  });
+
 });
